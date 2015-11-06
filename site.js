75657569cfe15067e7ea23a248fb064433823ca3
miles retuta <milesretuta@gmail.com>
2015-11-05 21:57:46 -0800

    first commit

---
+++ site.js
@@ -0,0 +1,48 @@
+function movieFunction(){
+	$('#results').html("");
+
+	var userInput = $("#searchform").val();
+
+	$.ajax({
+		type: "GET", 
+		dataType: "jsonp",
+		url: "http://api.themoviedb.org/3/search/movie",
+		data: "api_key=51a0abce642402e7b8d43b2081302e77&query="+userInput,
+		success: function(serverResponse){
+			console.log(serverResponse);
+			
+			var posterImg = "";
+				
+			for(var i = 0; i < serverResponse.results.length; i++){  					
+			
+			serverResponse.results[i].poster_path;  
+			
+			if(serverResponse.results[i].poster_path == null) {
+					posterImg = "img/nophoto.png";
+				} 
+
+				else{
+					posterImg = "http://image.tmdb.org/t/p/w154"+serverResponse.results[i].poster_path;
+				}
+
+
+			$("#results").append('<div class="movie"><img class="poster" src="'+posterImg+'"><div class="title">'+serverResponse.results[i].title+'</div><div class="rating">'+serverResponse.results[i].vote_average+'</div><div class="release">'+serverResponse.results[i].release_date+'</div></div>');	
+			};
+		}
+	});	
+}
+
+$(document).ready(function(){
+
+	$("#submit").click(function(){
+		movieFunction();
+	});
+
+	$('#searchform').keypress(function (e) {
+	  if (e.which == 13) {
+	  	movieFunction();	
+	  }
+	});
+});
+
+
